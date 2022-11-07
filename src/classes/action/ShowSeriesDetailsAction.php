8457a851ab14d27451758d588799926e9bf6df3f
Jules <jules.holder.54@gmail.com>
2022-11-07 15:30:39 +0100

    fix: fixed ShowSeriesDetailsAction.php structure

--- src/classes/action/ShowSeriesDetailsAction.php
+++ src/classes/action/ShowSeriesDetailsAction.php
@@ -3,6 +3,7 @@
 namespace iutnc\netvod\action;
 
 use iutnc\netvod\db\ConnectionFactory;
+use iutnc\netvod\lists\Serie;
 
 class ShowSeriesDetailsAction extends Action
 {
@@ -11,26 +12,25 @@ class ShowSeriesDetailsAction extends Action
     {
         $html = "";
         $pdo = ConnectionFactory::getConnection();
-        $querySeries = "SELECT * FROM serie WHERE id = :id";
-        $statement = $pdo->prepare($querySeries);
-        $statement->bindParam(":id", $_GET['id']);
-        $statement->execute();
-        $serie = $statement->fetchAll();
-        $html .= "<h1>{$serie['titre']}</h1>";
-        $html .= "<p>Résumé: {$serie['descriptif']}</p>";
-        $html .= "<p>Année: {$serie['annee']}</p>";
+        $serie = new Serie($_GET['id']);
+        $html .= "<h1>Titre:" . $serie->titre . "</h1>";
+        $html .= "<img src='assets/img/" . $serie->img . "' alt=''>";
+        $html .= "<p>Résumé:" . $serie->descriptif . "</p>";
+        $html .= "<p>Année: " . $serie->annee . "</p>";
+        $html .= "<p>Date d'ajout: " . $serie->date_ajout . "</p>";
+        $idS = $serie->id;
         $queryEpisodes = "SELECT * FROM episode WHERE serie_id = :id";
         $statement = $pdo->prepare($queryEpisodes);
-        $statement->bindParam(":id", $_GET['id']);
+        $statement->bindParam(":id", $idS);
         $statement->execute();
         $episodes = $statement->fetchAll();
         $nbepisodes = count($episodes);
-        $html .= "<p>$nbepisodes épisodes</p>";
+        $html .= "<p>" . $nbepisodes . " épisodes</p>";
         foreach ($episodes as $episode) {
-            $html .= "<p>Episode {$episode['numero']}</p>";
-            $html .= "<p>{$episode['titre']}</p>";
-            $html .= "<p>{$episode['resume']} minutes</p>";
-            $html .= "<p>{$episode['duree']}</p>";
+            $html .= "<p>Episode" . $episode['numero'] . "</p>";
+            $html .= "<p>" . $episode['titre'] . "</p>";
+            $html .= "<p>" . $episode['resume'] . " minutes</p>";
+            $html .= "<p>" . $episode['duree'] . "</p>";
         }
         return $html;
         // TODO: Implement execute() method.
