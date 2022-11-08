52755d7f93d5aedf23f2695dcf1ce1914bb9fd69
MaxenceIUT <117634432+MaxenceIUT@users.noreply.github.com>
2022-11-08 15:13:48 +0100

    chore: renamed manage account action to account action

--- src/classes/action/AccountAction.php
+++ src/classes/action/AccountAction.php
@@ -12,11 +12,30 @@ class AccountAction extends Action
 
             $html = <<<END
             <h1>Bonjour $user->first_name 👋</h1>
-            <a href="index.php?action=logout">Déconnexion</a>
-            <a href="index.php?action=manage-account">Gérer mon compte</a>
+            <h2>Gestion de mon compte</h2>
+            <form action="index.php?action=account" method="post">
+                <label for="first-name">Prénom</label>
+                <input type="text" name="first-name" id="first-name" placeholder="Prénom" value="$user->first_name" required>
+                <label for="last-name">Nom</label>
+                <input type="text" name="last-name" id="last-name" placeholder="Nom" value="$user->last_name" required>
+                <input type="submit" value="Mettre à jour">
+            </form>
             END;
 
             return $html;
+        } else if ($this->http_method == "POST") {
+            $user = $_SESSION['user'];
+            $_POST['first-name'] = filter_var($_POST['first-name'], FILTER_SANITIZE_STRING);
+            $_POST['last-name'] = filter_var($_POST['last-name'], FILTER_SANITIZE_STRING);
+
+            $user->first_name = $_POST['first-name'];
+            $user->last_name = $_POST['last-name'];
+            if ($user->save()) {
+                $_SESSION['user'] = $user;
+                return "Compte mis à jour";
+            } else {
+                return "Erreur lors de la mise à jour";
+            }
         } else {
             return "Méthode non autorisée";
         }
@@ -26,5 +45,4 @@ class AccountAction extends Action
     {
         return "account";
     }
-
 }
