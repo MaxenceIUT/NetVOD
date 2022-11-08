a4f8f6ab28cfc216bd22b0db74ddda1899f53dba
MaxenceIUT <117634432+MaxenceIUT@users.noreply.github.com>
2022-11-08 15:13:18 +0100

    feat: added register button in landing page

--- src/classes/action/LandingPageAction.php
+++ src/classes/action/LandingPageAction.php
@@ -11,6 +11,7 @@ class LandingPageAction extends Action
         <h1>Bienvenue sur NetVOD</h1>
         <p>NetVOD est la plateforme de SVOD la plus géniale du monde</p>
         <a href="index.php?action=login">Se connecter</a>
+        <a href="index.php?action=register">S'inscrire</a>
         END;
         return $html;
     }
