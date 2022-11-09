7e25bb823772918670643db447c4eb710dd2d479
Jules <jules.holder.54@gmail.com>
2022-11-09 11:44:35 +0100

    feat: added removeFavoriteSerieAction.php class #6

---
+++ src/classes/action/RemoveFavoriteSerieAction.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace iutnc\netvod\action;
+
+use iutnc\netvod\db\ConnectionFactory;
+
+class RemoveFavoriteSerieAction extends Action
+{
+
+    public function execute(): string
+    {
+        $html = "";
+        $pdo = ConnectionFactory::getConnection();
+        $query = "delete from favorite_series where email = :email and id = :id";
+        $statement = $pdo->prepare($query);
+        $user_email = $_SESSION['user']->email;
+        $statement->bindParam(":email", $user_email);
+        $id = $_GET['id'];
+        $statement->bindParam(":id", $id);
+        $statement->execute();
+        $html .= "Série supprimée des favoris";
+        return $html;
+    }
+
+    public function getActionName(): string
+    {
+        return "remove-favorite-serie";
+    }
+
+    public function shouldBeAuthenticated(): bool
+    {
+        return true;
+    }
+}
