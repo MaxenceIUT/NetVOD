f200b1ecefd8aac7d473a0c3e61cbef1b70f7ddf
Jules <jules.holder.54@gmail.com>
2022-11-09 11:44:26 +0100

    feat: added AddFavoriteSerieAction.php class #6

---
+++ src/classes/action/AddFavoriteSerieAction.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace iutnc\netvod\action;
+
+use iutnc\netvod\db\ConnectionFactory;
+
+class AddFavoriteSerieAction extends Action
+{
+
+
+    public function execute(): string
+    {
+        $html = "";
+        $pdo = ConnectionFactory::getConnection();
+        $query = "insert into favorite_series (email, id) values (:email, :id)";
+        $statement = $pdo->prepare($query);
+        $user_email = $_SESSION['user']->email;
+        $statement->bindParam(":email", $user_email);
+        $id = $_GET['id'];
+        $statement->bindParam(":id", $id);
+        $statement->execute();
+        $html .= "Série ajoutée aux favoris";
+        return $html;
+    }
+
+    public function getActionName(): string
+    {
+        return "add-favorite-serie";
+    }
+
+    public function shouldBeAuthenticated(): bool
+    {
+        return true;
+    }
+}
