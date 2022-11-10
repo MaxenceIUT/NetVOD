5661bf2b8505b209213c09267946f2c061a8212f
MaxenceIUT <117634432+MaxenceIUT@users.noreply.github.com>
2022-11-10 19:47:51 +0100

    refactor: moved form to a dedicated method and showing it again in case of an error

--- src/classes/action/account/AccountAction.php
+++ src/classes/action/account/AccountAction.php
@@ -10,40 +10,45 @@ use PDO;
 class AccountAction extends Action
 {
 
-    public function execute(): string
+    private string $form;
+
+    private function generateForm()
     {
-        if ($this->http_method == "GET") {
-            $user = Auth::getCurrentUser();
+        $user = Auth::getCurrentUser();
 
-            $html = <<<END
-            <h1>Bonjour $user->first_name 👋</h1>
-            <h2>Gestion de mon compte</h2>
-            <form action="index.php?action=account" method="post">
-                <label for="first-name">Prénom</label>
-                <input type="text" name="first-name" id="first-name" placeholder="Prénom" value="$user->first_name" required>
-                <label for="last-name">Nom</label>
-                <input type="text" name="last-name" id="last-name" placeholder="Nom" value="$user->last_name" required>
-                <label for="favorite-genre">Genre préféré</label>
-            END;
+        $this->form = <<<END
+        <h1>Gestion de mon compte</h1>
+        <form action="index.php?action=account" method="post">
+            <label for="first-name">Prénom</label>
+            <input type="text" name="first-name" id="first-name" placeholder="Prénom" value="$user->first_name" required>
+            <label for="last-name">Nom</label>
+            <input type="text" name="last-name" id="last-name" placeholder="Nom" value="$user->last_name" required>
+            <label for="favorite-genre">Genre préféré</label>
+        END;
 
-            $genres = $this->getAvailableGenres();
-            $html .= "<select name=\"favorite-genre\" id=\"favorite-genre\">";
+        $genres = $this->getAvailableGenres();
+        $this->form .= "<select name=\"favorite-genre\" id=\"favorite-genre\">";
 
-            $selected = $user->favorite_genre == null ? "selected" : "";
-            $html .= "<option value=\"\" $selected>Ne pas spécifier</option>";
+        $selected = $user->favorite_genre == null ? "selected" : "";
+        $this->form .= "<option value=\"\" $selected>Ne pas spécifier</option>";
 
-            foreach ($genres as $genre) {
-                $selected = $genre['id'] == $user->favorite_genre ? "selected" : "";
-                $html .= "<option value=\"{$genre['id']}\" $selected>{$genre['genre']}</option>";
-            }
+        foreach ($genres as $genre) {
+            $selected = $genre['id'] == $user->favorite_genre ? "selected" : "";
+            $this->form .= "<option value=\"{$genre['id']}\" $selected>{$genre['genre']}</option>";
+        }
 
-            $html .= <<<END
+        $this->form .= <<<END
                 </select>
                 <input type="submit" value="Mettre à jour">
             </form>
             END;
+    }
 
-            return $html;
+    public function execute(): string
+    {
+        $this->generateForm();
+        if ($this->http_method == "GET") {
+            return $this->form;
         } else if ($this->http_method == "POST") {
             $user = Auth::getCurrentUser();
 
@@ -55,10 +60,9 @@ class AccountAction extends Action
             $user->favorite_genre = $_POST['favorite-genre'] == "" ? null : $_POST['favorite-genre'];
 
             if ($user->save()) {
-                $_SESSION['user'] = $user;
-                return "Compte mis à jour";
+                return "Compte mis à jour" . $this->form;
             } else {
-                return "Erreur lors de la mise à jour";
+                return "Erreur lors de la mise à jour" . $this->form;
             }
         } else {
             return "Méthode non autorisée";
