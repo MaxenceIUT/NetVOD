8b4352b3e23a7388312523606d6c4b09121f1ce5
MaxenceIUT <117634432+MaxenceIUT@users.noreply.github.com>
2022-11-10 13:01:57 +0100

    feat: improved landing page by showing the user he's already logged in

--- src/classes/action/LandingPageAction.php
+++ src/classes/action/LandingPageAction.php
@@ -2,17 +2,33 @@
 
 namespace iutnc\netvod\action;
 
+use iutnc\netvod\auth\Auth;
+
 class LandingPageAction extends Action
 {
 
     public function execute(): string
     {
+        $user = Auth::getCurrentUser();
         $html = <<<END
         <h1>Bienvenue sur NetVOD</h1>
-        <p>NetVOD est la plateforme de SVOD la plus géniale du monde</p>
-        <a href="index.php?action=login">Se connecter</a>
-        <a href="index.php?action=register">S'inscrire</a>
+        <h2>NetVOD est la plateforme de SVOD la plus géniale du monde</h2>
         END;
+
+        if ($user == null) {
+            $html .= <<<END
+            <a href="index.php?action=login">Se connecter</a>
+            <a href="index.php?action=register">S'inscrire</a>
+            END;
+        } else {
+            $html .= <<<END
+            <div class="account">
+                <p>Bonjour $user->first_name 👋</p>
+                <a href="index.php?action=home">Mon compte</a>
+                <a href="index.php?action=logout">Se déconnecter</a>
+            </div>
+            END;
+        }
         return $html;
     }
 
