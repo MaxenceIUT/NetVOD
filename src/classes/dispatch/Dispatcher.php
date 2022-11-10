9cebeb092efa2c32eb02738913213095e4f36e7c
MaxenceIUT <117634432+MaxenceIUT@users.noreply.github.com>
2022-11-10 13:20:10 +0100

    fix: css href

--- src/classes/dispatch/Dispatcher.php
+++ src/classes/dispatch/Dispatcher.php
@@ -94,7 +94,7 @@ class Dispatcher
             <head>
                 <meta charset="UTF - 8">
                 <title>NetVOD</title>
-                <link rel="stylesheet" href="assets / css / style . css">
+                <link rel="stylesheet" href="assets/css/style.css">
             </head>
             <body>
         END;
